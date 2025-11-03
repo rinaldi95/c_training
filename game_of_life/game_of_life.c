465970a2a56cf1aad81687ce52bc9a019be62c8a
roberto <rinaldiroberto95@gmail.com>
2025-11-03 12:30:22 +0100

    malloc in game of life

--- game_of_life/game_of_life.c
+++ game_of_life/game_of_life.c
@@ -1,36 +1,46 @@
 #include<stdio.h>
-#include<unistd.h>
+#include<stdlib.h>
+#include<unistd.h> 
+
 int main(void){
-	int col=35;
-	int row=30;
-	int nmax=1000;
-	int mi[col*row];
-	int mf[col*row];
-	int *tp;
-	int *pmi=mi;
-	int *pmf=mf;
-	int di[8]={row-1,row-1,row-1,0,0,1,1,1};
-	int dj[8]={col-1,0,1,col-1,1,col-1,0,1};
-	for (int i=0;i<row*col;i++) mf[i]=0;
-	for (int i=0;i<row*col-566;i++) mf[i]=1;
-	for (int n=0;n<nmax;n++){
-		tp=pmi;
-		pmi=pmf;
-		pmf=tp;
+	int col=20;
+	int row=15;
+	int *mi = malloc(col*row*sizeof(int));
+	int *mf= malloc(col*row*sizeof(int));
+	if (mi==NULL||mf==NULL){
+		perror("malloc failed");
+		return 1;
+	}
+	for (int i=0;i<66; i++) mf[i]=1;
+	int *temp;
+	int di[8]={1,1,1,0,0,row-1,row-1,row-1};
+	int dj[8]={1,0,col-1,1,col-1,1,0,col-1};
+	for (int n=0;n<5;n++){
+		temp=mi;
+		mi=mf;
+		mf=temp;
 		printf("\033[2J\033[H");
 		for (int i=0;i<row;i++){
-			for(int j=0;j<col;j++){
-				pmf[i*col+j]=0;
+			for(int j=0;j<row;j++){
+				mf[i*col+j]=0;
 				for (int k=0;k<8;k++){
-					pmf[i*col+j]+=pmi[(i+di[k])%row*col+(j+dj[k])%col];
-					}
-				printf("%s",(pmi[i*col+j]==0)? ".." : "##");
-				if (pmi[i*col+j]==0) pmf[i*col+j]=(pmf[i*col+j]==3);
-				else pmf[i*col+j]=(pmf[i*col+j]==2 || pmf[i*col+j]==3);
+					mf[i*col+j]+=mi[(i+di[k])%row*col+(j+dj[k])%col];
+				}
+				if (mi[i*col+j]==1) {
+					mf[i*col+j]=(mf[i*col+j]==2||mf[i*col+j]==3);
+					printf("##");
+				}
+				else {
+					mf[i*col+j]=(mf[i*col+j]==3);
+					printf("..");
+				}
+				
 			}
 			printf("\n");
 		}
-	usleep(200000);
+		usleep(500000);
 	}
 	return 0;
 }
+			
+				
