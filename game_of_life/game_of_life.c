d3a1abb7a35b7ade9d1f67fc67c8a8c5b847091c
roberto <rinaldiroberto95@gmail.com>
2025-11-06 01:06:17 +0100

    file header in game of life

--- game_of_life/game_of_life.c
+++ game_of_life/game_of_life.c
@@ -1,40 +1,40 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<stdlib.h>
-#include<unistd.h> 
 
 int main(void){
-	int col=20;
 	int row=15;
-	int *mi = malloc(col*row*sizeof(int));
-	int *mf= malloc(col*row*sizeof(int));
-	if (mi==NULL||mf==NULL){
-		perror("malloc failed");
-		return 1;
+	int col=20;
+	int *mi=malloc(row*col*sizeof(int));
+	int *mf=malloc(row*col*sizeof(int));
+	if (mi==NULL || mf ==NULL ){
+		perror("malloc failure");
+		return 1;	
 	}
-	for (int i=0;i<66; i++) mf[i]=1;
+	for(int i=0;i<45;i++) mf[i]=1;
 	int *temp;
 	int di[8]={1,1,1,0,0,row-1,row-1,row-1};
 	int dj[8]={1,0,col-1,1,col-1,1,0,col-1};
-	for (int n=0;n<5;n++){
+	for (int n=0; n<10; n++){
 		temp=mi;
 		mi=mf;
 		mf=temp;
 		printf("\033[2J\033[H");
-		for (int i=0;i<row;i++){
-			for(int j=0;j<row;j++){
+		for (int i =0 ; i<row; i++){
+			for(int j=0;j<col; j++){
 				mf[i*col+j]=0;
-				for (int k=0;k<8;k++){
+				for(int k=0; k<8;k++){ 
 					mf[i*col+j]+=mi[(i+di[k])%row*col+(j+dj[k])%col];
 				}
-				if (mi[i*col+j]==1) {
-					mf[i*col+j]=(mf[i*col+j]==2||mf[i*col+j]==3);
+				if (mi[i*col+j]==1){
+					mf[i*col+j]=(mf[i*col+j]==2 || mf[i*col+j]==3);
 					printf("##");
 				}
-				else {
+				else{
 					mf[i*col+j]=(mf[i*col+j]==3);
 					printf("..");
 				}
-				
+			
 			}
 			printf("\n");
 		}
@@ -42,5 +42,4 @@ int main(void){
 	}
 	return 0;
 }
-			
-				
+	
